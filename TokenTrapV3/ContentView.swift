059d428940894a6b93bd6fa477c3ba843763fdc0
Ben <ben@mylikes.com>
2022-07-31 11:21:05 -0400

    Initial Commit

---
+++ TokenTrapV3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TokenTrapV3
+//
+//  Created by Ben Balcomb on 7/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TokenTrapV3/TokenTrapV3App.swift
@@ -0,0 +1,17 @@
+//
+//  TokenTrapV3App.swift
+//  TokenTrapV3
+//
+//  Created by Ben Balcomb on 7/31/22.
+//
+
+import SwiftUI
+
+@main
+struct TokenTrapV3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
