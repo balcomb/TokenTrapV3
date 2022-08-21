b4d43cad6272f908167ef18f2dd8ddecf1182229
Ben <ben@mylikes.com>
2022-08-21 18:12:34 -0400

    add board grid

--- TokenTrapV3/Color+Extensions.swift
+++ TokenTrapV3/Color+Extensions.swift
@@ -11,4 +11,6 @@ extension Color {
     static var background: Color { Color("Background") }
     static var buttonBlue: Color { Color("ButtonBlue") }
     static var logoBlue: Color { Color("LogoBlue") }
+    static var boardBackground: Color { Color("BoardBackground") }
+    static var gridBackground: Color { Color("GridBackground") }
 }
--- TokenTrapV3/GameView.swift
+++ TokenTrapV3/GameView.swift
@@ -10,8 +10,79 @@ import SwiftUI
 struct GameView: View {
     let settings: GameSettings
     var completion: () -> Void
+
+    static var boardWidth: CGFloat {
+        let screenWidth = UIScreen.screens.first?.bounds.size.width ?? .zero
+        guard UIDevice.current.userInterfaceIdiom == .pad else {
+            return screenWidth
+        }
+        return screenWidth * 0.666
+    }
+
     var body: some View {
-        Button("dismiss") { completion() }
+        VStack(spacing: 0) {
+            topControls
+            boardContainer
+            bottomControls
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.background)
+    }
+
+    private var topControls: some View {
+        VStack {
+            Spacer()
+            Button("dismiss") { completion() }.foregroundColor(.yellow)
+            Spacer()
+        }
+    }
+
+    private var boardContainer: some View {
+        VStack(spacing: 0) {
+            GridView()
+        }
+        .frame(width: Self.boardWidth, height: Self.boardWidth)
+        .background(Color.boardBackground)
+    }
+
+    private var bottomControls: some View {
+        VStack {
+            Spacer()
+            Text("this is it")
+            Spacer()
+        }
+    }
+}
+
+struct GridView: View {
+    private var gridSize: Int { 8 }
+    private var adjustedWidth: CGFloat { GameView.boardWidth - (2 * cellSpacing) }
+    private var cellSpacing: CGFloat { 1 }
+    private var cellSize: CGFloat { (adjustedWidth / CGFloat(gridSize)) - cellSpacing }
+
+    var body: some View {
+        VStack(spacing: cellSpacing) {
+            fill(with: { row })
+        }
+        .frame(width: adjustedWidth, height: adjustedWidth)
+    }
+
+    private var row: some View {
+        HStack(spacing: cellSpacing) {
+            fill(with: { cell })
+        }
+    }
+
+    private var cell: some View {
+        Circle()
+            .foregroundColor(.gridBackground)
+            .frame(width: cellSize, height: cellSize)
+    }
+
+    private func fill<Content: View>(with content: @escaping () -> Content) -> some View {
+        ForEach(1...gridSize, id: \.self) { _ in
+            content()
+        }
     }
 }
 
